1904abbac25b49e26276013ee5a4ce41c70eb568
YooJeong01 <ujjh77@naver.com>
2025-06-11 19:37:34 +0900

    [Refactor] 불필요한 파라미터 수정

--- script.js
+++ script.js
@@ -6,35 +6,45 @@ import { initTheme } from "./lib/theme.js";
 import { bindEvents } from "./lib/event.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadFromLocalStorage();
-  const setTheme = initTheme();
-  initChart();
-  setTheme(state.appData.theme || "light");
-  init();
-  bindEvents();
-  getToday();
+    loadFromLocalStorage();
+    const setTheme = initTheme();
+    initChart();
+    setTheme(state.appData.theme || "light");
+    init();
+    bindEvents();
+    getToday();
 });
 
 function init() {
-  const todoList = document.querySelector(".todoList");
-  const scheduleList = document.querySelector(".scheduleList");
+    const todoList = document.querySelector(".todoList");
+    const scheduleList = document.querySelector(".scheduleList");
 
-  state.todoListArray.forEach(item => {
-    renderItem({ type: "todo", target: todoList, item, isInit: true });
+    state.todoListArray.forEach(item => {
+        renderItem({ 
+            type: "todo", 
+            target: todoList, 
+            item, 
+            isInit: true 
+        });
   });
 
-  state.scheduleArray.forEach(item => {
-    renderItem({ type: "schedule", target: scheduleList, item, isInit: true });
-  });
+    state.scheduleArray.forEach(item => {
+        renderItem({ 
+            type: "schedule", 
+            target: scheduleList, 
+            item, 
+            isInit: true 
+        });
+    });
 }
 
 function getToday() {
-  const todayDate = document.querySelector(".todayDate");
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const date = String(today.getDate()).padStart(2, "0");
-  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  const day = days[today.getDay()];
-  todayDate.textContent = `${year}. ${month}. ${date} ${day}`;
+    const todayDate = document.querySelector(".todayDate");
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const date = String(today.getDate()).padStart(2, "0");
+    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const day = days[today.getDay()];
+    todayDate.textContent = `${year}. ${month}. ${date} ${day}`;
 }
